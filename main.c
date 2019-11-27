985d0c4a1d00fc7097d3209c9d61ea6f50227a27
seedmonn <seedmp322@yandex.ru>
2019-11-27 14:29:46 +0300

    finalo4ka

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #define YES 1
 #define NO 0
 
@@ -11,10 +11,6 @@ int main()
 	int cnt=0;
 	int flag = NO;
 	int found = NO;
-	char* j = arr; // позиция начала слова
-	char* in_ptr = arr; // (i) - позиция текущего символа
-	char* out_ptr = arr; // (pos) - позиция символа выхода
-	char* word_ptr = arr; // (start) - позиция начала слова
 
 	FILE* in;
 	in = fopen("text.txt", "r");
@@ -25,11 +21,16 @@ int main()
 	}
 	FILE* out;
 	out = fopen("result.txt", "w");
-
+	while(!feof(in))
+	{
+		memset(arr, 0, 100 * sizeof(arr[0]));
 		fgets(arr, 100, in);
+		char* j = arr; // позиция начала слова
+		char* in_ptr = arr; //  позиция текущего символа
+		char* word_ptr = arr; // позиция начала слова
 		do
 		{
-			c = *in_ptr; // взять текущий символ из буфера
+			c = *in_ptr; // взять текущий символ 
 			if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && (c != ' ' && c != '.' && c != ',' && c != '\n' && c != '\0' && c != '\t')) // проверка на не слово
 			{
 				found = YES;
@@ -46,16 +47,18 @@ int main()
 						char c3 = *(j + 2);
 						if ((c1 == 'o' && c2 == 'e') && (found != YES) && (c3 == ' ' || c3 == '.' || c3 == ',' || c3 == '\0' || c3 == '\n'))
 						{// проверка на: 1)заглавные буквы и условие задачи. 2) условие является ли слово "словом".
-							printf("strt:->");
+							printf("strt:->"); fprintf(out, "strt:->");
 							for (j = word_ptr; j < in_ptr; j++)
-								printf("%c", *j);
+							{
+								printf("%c", *j); fputc(*j, out);
+							}
 							break;
 						}
 					}
 				}
 				flag = NO;
 				found = NO;
-				printf("%c", *in_ptr); //пробелы для "нормальных слов" (по условию задачи)
+				printf("%c", *in_ptr); fputc(*in_ptr, out); //пробелы для "нормальных слов" (по условию задачи)
 			}
 			else
 			{
@@ -64,7 +67,8 @@ int main()
 				flag = YES; //признак слова
 			}
 			in_ptr++; //переход к следующей ячейке буфера
-		} while (c != '\0');	
+		} while (c != '\0');
+	}
 	fclose(in);
 	fclose(out);
 	return 0;
