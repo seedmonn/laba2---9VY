27d5ebad40d72facf9b3451544f1c600fa5dc4a2
seedmonn <seedmp322@yandex.ru>
2019-12-04 21:58:51 +0300

    lol

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #define YES 1
 #define NO 0
+#define LENGTH 100
 
 int main()
 {
 	char c;
-	char arr[100];
+	char str[LENGTH];
 	int cnt=0;
 	int flag = NO;
 	int found = NO;
+	char *p = str;
 
 	FILE* in;
 	in = fopen("text.txt", "r");
@@ -23,15 +25,15 @@ int main()
 	out = fopen("result.txt", "w");
 	while(!feof(in))
 	{
-		memset(arr, 0, 100 * sizeof(arr[0]));
-		fgets(arr, 100, in);
-		char* j = arr; // позиция начала слова
-		char* in_ptr = arr; //  позиция текущего символа
-		char* word_ptr = arr; // позиция начала слова
+		memset(str, 0, LENGTH * sizeof(str[0]));
+		fgets(str, LENGTH, in);
+		char* j = str; // позиция начала слова
+		char* in_ptr = str; //  позиция текущего символа
+		char* word_ptr = str; // позиция начала слова
 		do
 		{
 			c = *in_ptr; // взять текущий символ 
-			if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && (c != ' ' && c != '.' && c != ',' && c != '\n' && c != '\0' && c != '\t')) // проверка на не слово
+			if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && (c != ' ' && c != '.' && c != ',' && c != '\n' && c != '\0' && c != '\t' && c != '>' )) // проверка на не слово
 			{
 				found = YES;
 			}
@@ -43,11 +45,10 @@ int main()
 					for (j = word_ptr; j < in_ptr - 1; j++)
 					{
 						char c1 = *j;
-						char c2 = *(j + 1);
-						char c3 = *(j + 2);
-						if ((c1 == 'o' && c2 == 'e') && (found != YES) && (c3 == ' ' || c3 == '.' || c3 == ',' || c3 == '\0' || c3 == '\n'))
+						char c2 = *(j + 1);  
+						char c3 = *(j + 2); 
+						if (((c1 == 'o' && c2 == 'e') || (c1 == 'o' && c2 == 'E') || (c1 == 'O' && c2 == 'e') || (c1 == 'O' && c2 == 'E')) && (found != YES) && (c3 == ' ' || c3 == '.' || c3 == ',' || c3 == '\0' || c3 == '\n'))
 						{// проверка на: 1)заглавные буквы и условие задачи. 2) условие является ли слово "словом".
-							printf("strt:->"); fprintf(out, "strt:->");
 							for (j = word_ptr; j < in_ptr; j++)
 							{
 								printf("%c", *j); fputc(*j, out);
@@ -58,6 +59,8 @@ int main()
 				}
 				flag = NO;
 				found = NO;
+				if (*in_ptr == '.' || *in_ptr == ',') // для нормального отображения в консоли
+					*in_ptr = ' ';
 				printf("%c", *in_ptr); fputc(*in_ptr, out); //пробелы для "нормальных слов" (по условию задачи)
 			}
 			else
@@ -69,6 +72,7 @@ int main()
 			in_ptr++; //переход к следующей ячейке буфера
 		} while (c != '\0');
 	}
+	printf("\n");
 	fclose(in);
 	fclose(out);
 	return 0;
